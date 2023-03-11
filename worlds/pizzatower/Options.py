96fe1d1b36a6b95de3835afaf2a5fd73386e4204
bbs <babyblueysheep@gmail.com>
2023-03-11 22:31:30 +0200

    buncha new options

--- worlds/pizzatower/Options.py
+++ worlds/pizzatower/Options.py
@@ -1,5 +1,39 @@
 import typing
-from Options import Choice, Toggle, Option, DeathLink
+from Options import Choice, Toggle, Option, DeathLink, Range
+
+
+class BaseTrapWeight(Choice):
+    """
+    Base Class for Trap Weights
+    """
+    option_none = 0
+    option_low = 1
+    option_medium = 2
+    option_high = 4
+    default = 2
+
+
+class TimerTrap(BaseTrapWeight):
+    """Likelihood of a receiving a trap which starts Pizza Time earlier than usual."""
+    display_name = "Timer Trap Weight"
+
+
+class StunTrap(BaseTrapWeight):
+    """Likelihood of a receiving a trap which stuns you temporarily."""
+    display_name = "Stun Trap Weight"
+
+
+class TransformationTrap(BaseTrapWeight):
+    """Likelihood of a receiving a trap which gives you a random transformation."""
+    display_name = "Transformation Trap Weight"
+
+
+class TrapFillPercentage(Range):
+    """Replace a percentage of junk items in the item pool with random traps."""
+    display_name = "Trap Fill Percentage"
+    range_start = 0
+    range_end = 100
+    default = 0
 
 
 class LevelShuffle(Toggle):
@@ -16,6 +50,7 @@ class RankRequirement(Choice):
     option_a = "A"
     option_s = "S"
     option_p = "P"
+    default = option_d
 
 
 class JudgementRequirement(Choice):
@@ -27,6 +62,7 @@ class JudgementRequirement(Choice):
     option_no_judgement = 3
     option_not_bad_not_bad_at_all = 4
     option_wow = 5
+    default = option_you_suck
 
 
 class JohnNeeded(Toggle):
@@ -34,10 +70,24 @@ class JohnNeeded(Toggle):
     display_name = "Required John"
 
 
+class SecretLocation(Toggle):
+    """Whether secret entrances grant checks."""
+    display_name = "Secret Checks"
+
+
 pizza_tower_options: typing.Dict[str, type(Option)] = {
     "death_link": DeathLink,
+
+    "secret_check": SecretLocation,
+
+    "timer_trap": TimerTrap,
+    "stun_trap": StunTrap,
+    "transformation_trap": TransformationTrap,
+    "trap_fill_percentage": TrapFillPercentage,
+
     "shuffle_level": LevelShuffle,
+
     "rank_needed": RankRequirement,
+    "goal": JudgementRequirement,
     "john": JohnNeeded,
-    "goal": JudgementRequirement
 }
