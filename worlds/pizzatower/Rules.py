9006b0aa7edce3689a5ed995edb20e9877f73353
bbs <babyblueysheep@gmail.com>
2023-03-14 19:28:01 +0200

    added rules for other keys

--- worlds/pizzatower/Rules.py
+++ worlds/pizzatower/Rules.py
@@ -11,7 +11,17 @@ class PizzaTowerLogic(LogicMixin):
 
 def set_rules(world: MultiWorld, player: int):
     for name, data in task_table.items():
-        if data.region in Regions.floor_1_regions: continue
+        if data.region in Regions.floor_1_regions:
+            continue
         forbid_item(world.get_location(name, player), Names.pepperman + " Boss Key", player)
+        if data.region in Regions.floor_2_regions:
+            continue
+        forbid_item(world.get_location(name, player), Names.vigilante + " Boss Key", player)
+        if data.region in Regions.floor_3_regions:
+            continue
+        forbid_item(world.get_location(name, player), Names.noise + " Boss Key", player)
+        if data.region in Regions.floor_4_regions:
+            continue
+        forbid_item(world.get_location(name, player), Names.fakepep + " Boss Key", player)
 
     world.completion_condition[player] = lambda state: state.has("Victory", player)
